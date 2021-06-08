1b681cf001a57bdab0e1820be61f5a8f4bf4f51d
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-09 02:22:10 +0530

    complexstateobjects,multiple reducers, with useContext, fetching data

--- reducer-hooks/src/App.js
+++ reducer-hooks/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 function App() {
   return (
     <div className="App">
+      <DataFetchingTwo />
       {/* <CounterOne /> */}
-      <CounterTwo />
+      {/* <CounterTwo />  */}
+      {/* <CounterThree /> */}
     </div>
   );
 }
